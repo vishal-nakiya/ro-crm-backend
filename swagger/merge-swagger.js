63dec45b0501de4f8051e885330b9c5fd57cad9f
vishalnakiya1 <vishal.nakiya@tuvoc.com>
2025-07-08 18:49:18 +0530

    changes in swagger

--- swagger/merge-swagger.js
+++ swagger/merge-swagger.js
@@ -13,7 +13,7 @@ function mergeSwaggerFiles() {
 
     // Get all YAML files in the swagger directory
     const files = fs.readdirSync(swaggerDir)
-        .filter(file => file.endsWith('.yaml') && file !== 'main.yaml');
+        .filter(file => file.endsWith('.yaml') && file !== 'main.yaml' && file !== 'merged-swagger.yaml');
 
     // Merge paths from all files
     files.forEach(file => {
@@ -29,14 +29,23 @@ function mergeSwaggerFiles() {
         }
     });
 
-    // Write merged file
+    // Generate merged content in memory
     const mergedContent = yaml.dump(mainSwagger, {
         lineWidth: -1,
         noRefs: true
     });
 
-    fs.writeFileSync(path.join(swaggerDir, 'merged-swagger.yaml'), mergedContent);
-    console.log('Swagger files merged successfully!');
+    // Try to write merged file, but don't fail if file system is read-only
+    try {
+        fs.writeFileSync(path.join(swaggerDir, 'merged-swagger.yaml'), mergedContent);
+        console.log('Swagger files merged successfully!');
+    } catch (error) {
+        if (error.code === 'EROFS' || error.code === 'EACCES') {
+            console.log('Swagger files merged in memory (read-only file system detected)');
+        } else {
+            console.error('Error writing merged swagger file:', error.message);
+        }
+    }
 
     return mainSwagger;
 }
@@ -52,11 +61,14 @@ function setupSwagger(app) {
         customSiteTitle: 'RO CRM API Documentation'
     }));
 
-    // Serve raw YAML file
+    // Serve raw YAML file - generate on-the-fly instead of reading from disk
     app.get('/api-docs.yaml', (req, res) => {
-        const yamlContent = fs.readFileSync(path.join(__dirname, 'merged-swagger.yaml'), 'utf8');
+        const mergedContent = yaml.dump(mergedSwagger, {
+            lineWidth: -1,
+            noRefs: true
+        });
         res.setHeader('Content-Type', 'text/yaml');
-        res.send(yamlContent);
+        res.send(mergedContent);
     });
 }
 
